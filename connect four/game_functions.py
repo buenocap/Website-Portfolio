6709e2f8811c527caa6f64fb6eacd2a20bb7d3cd
Pedro Bueno <pedro.buenocastaneda@ucdenver.edu>
2023-06-26 20:20:41 -0600

    Naming Convention Changed the name of some functions to accurately follow naming convention standards Fixed place_token Placed token now works as intended

--- connect four/game_functions.py
+++ connect four/game_functions.py
@@ -1,12 +1,20 @@
 """
+
 Connect Four
+
 Pedro Bueno Castaneda
+
 This file provides all functionality needed to properly run the game in the user's terminal.
+
 """
+
 import numpy as np
 
+
 #Initialize a profile for the player
+
 class player:
+
     def __init__(self,token,turn=0,score=0):
         self.token = token;
         self.turn = turn;
@@ -14,21 +22,25 @@ class player:
     
     def get_token(self):
         return self.token;
+
     def set_token(self, x):
         self.token = x;
         
     def get_turn(self):
         return self.turn;
+
     def set_turn(self, x):
         self.turn = x;
         
     def get_score(self):
         return self.score;
+
     def set_score(self, x):
         self.score = x;
 
 #Initialize a profile for the computer
 class computer:
+
     def __init__(self,token,turn=0,score=0):
         self.token = token;
         self.turn = turn;
@@ -36,66 +48,66 @@ class computer:
 
     def get_token(self):
         return self.token;
+
     def set_token(self, x):
         self.token = x;
         
     def get_turn(self):
         return self.turn;
+
     def set_turn(self, x):
         self.turn = x;
         
     def get_score(self):
         return self.score;
+
     def set_score(self, x):
         self.score = x;
 
 #Initalize a profile for the game
 class Tic_Tac_Toe:
+
     def __init__(self,game_over = False, game_board =[]):
         self.game_status = game_over;
         self.game_board = game_board;
         
     def get_game_status(self):
         return self.game_status;
-        
-    def createBoard(self):
+
+    def create_board(self):
         game_board = np.zeros((6,7));
         self.game_board = game_board;
         return game_board;
-    
-    def displayBoard(self):
+
+    def display_board(self):
         print(" 1  2  3  4  5  6  7");
         print(" ---------------------");
         for x in self.game_board:
             print(x)
         return 0;
-    
-    def placeToken(self,token):
-        x = 5;
+
+# Allow the player to place a token on the game board
+    def place_token(self,token):
+        game_board_length = len(self.game_board);
         valid = False;
         placement = int(input("Player {token} please enter number 1-7: ".format(token=token)))-1;
-        
         print("\n");
-        if(placement < 7 and placement > -1):
-            while(valid != True):
+
+        if(placement > -1 and placement < 7):
+            for x in reversed(range(-1,game_board_length)):
+                if x == -1:
+                    print("Row is full made another selection!");
+                    self.display_board();
+                    self.place_token(token);
                 if(self.game_board[x][placement] == 0):
                     self.game_board[x][placement] = token;
-                    valid = True;
-                elif (x == -1):
-                    self.displayBoard();
-                    print("Row is full please try again");
-                    self.placeToken(token);
-                else:
-                    x -= 1;  
-        else:
-            self.displayBoard();
-            print("Invalid selection, please try again") 
-            self.placeToken(token);
-        self.displayBoard();                 
-        return 0;
-    
-    def checkWin(self, currentX, currentY, player):
+                    self.display_board();
+                    return 0
+        
+    def check_win(self, currentX, currentY, player):
+
         tracker = 0;
+
         #Checking left win condition
         while(currentY > -1):
             if(self.game_board[currentX][currentY] == 0):
@@ -104,6 +116,3 @@ class Tic_Tac_Toe:
             if(tracker == 4):
                 print("Player {player} has won the match!".format(player.get_token()));
                 self.game_status = True;
-        
-        
-        
--- connect four/game_type.py
+++ connect four/game_type.py
@@ -1,14 +1,18 @@
 import game_functions
 
+
 def PvP(game):
+
     player_one = game_functions.player(1);
     player_two = game_functions.player(2);
-    game.createBoard();
-    game.displayBoard();
+    game.create_board();
+    game.display_board();
     while(game.game_status == False):
-        game.placeToken(player_one.get_token());
-        game.placeToken(player_two.get_token());
+        game.place_token(player_one.get_token());
+        game.check_win();
+        game.place_token(player_two.get_token());
     return 0;
+
 
 def PvC(game):
     print("Player Vs Computer");
